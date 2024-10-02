d89834d26bf43750e508e0ad02349cc01577feae
unknown <an.lehonghcm@gmail.com>
2024-10-02 13:42:19 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-chat-application",
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
